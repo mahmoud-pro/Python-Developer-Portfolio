77a8708fc67baa16d8e86fd6c430cc71c6a751d6
mahmoud-pro <56353916+mahmoud-pro@users.noreply.github.com>
2023-05-17 07:42:42 +0300

    add weather forcast project

--- Home.py
+++ Home.py
@@ -23,14 +23,14 @@ col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
 df = pd.read_csv("assets/data.csv", sep=";")
 
 with col3:
-    for index, row in df[:10].iterrows():
+    for index, row in df[:11].iterrows():
         st.header(row["title"])
         st.write(row["description"])
         st.image("assets/images/" + row['image'])
         st.write(f"[Source Code]({row['url']})")
 
 with col4:
-    for index, row in df[10:].iterrows():
+    for index, row in df[11:].iterrows():
         st.header(row["title"])
         st.write(row["description"])
         st.image("assets/images/" + row['image'])
